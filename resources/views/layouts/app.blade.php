43e3f53dc88844c0f273b0778a8f6a9e94fd3089
xcvbn13 <withoutweed@student.telkomuniversity.ac.id>
2022-06-13 09:36:11 +0700

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,11 @@
     {{-- icon  --}}
     <script src="https://kit.fontawesome.com/9ab8981530.js" crossorigin="anonymous"></script>
 
+    <!-- add icon link -->
+    <link rel = "icon" href = 
+    "{{ asset('assets/img/logo_title.png') }}" 
+            type = "image/x-icon">
+
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
