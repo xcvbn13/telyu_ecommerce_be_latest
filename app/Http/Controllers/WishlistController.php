c7deddbc440ef44a3bab89390221d8e505dd3484
xcvbn13 <withoutweed@student.telkomuniversity.ac.id>
2022-06-21 07:09:56 +0700

    make it better

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -14,7 +14,7 @@ class WishlistController extends Controller
      */
     public function index()
     {
-        $wishlist = Wishlist::where('id_user',auth()->user()->id)->with(['product','user'])->get();
+        $wishlist = Wishlist::where('id_user',auth()->user()->id)->with(['user','product'])->get();
 
         return response([
             'message' => "Berhasil mengambil data wishlist",
