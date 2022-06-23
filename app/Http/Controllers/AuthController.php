11b49f68bcd24e8551f931c765734797d18f7ddc
xcvbn13 <withoutweed@student.telkomuniversity.ac.id>
2022-06-23 08:31:31 +0700

    make it better

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     public function login(Request $request){
         $request->validate([
             'email' => 'required|email',
-            'password' => 'required|current_password',
+            'password' => 'required|current_password:users',
         ]);
 
         // $remember_me = true;
