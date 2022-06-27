4cb7074d915b7f2d0a59733c1c5525ff15415bc8
xcvbn13 <withoutweed@student.telkomuniversity.ac.id>
2022-06-27 11:51:41 +0700

    make it better

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,16 +25,16 @@ class OrderController extends Controller
     public function index()
     {
         $cart = Cart::where('id_user',auth()->user()->id)->where('id_status_cart',2)->get();
-        // $review = array();
-        $order = Order::where('id_cart',$cart->id)->get();
+        $review = array();
+        // $order = Order::where('id_cart',$cart->id)->get();
 
-        // foreach ($cart as $key => $value) {
-        //     $order = Order::where('id_cart',$value['id'])->first();
-        //     $review[] = [$order];
-        // }
+        foreach ($cart as $key => $value) {
+            $order = Order::where('id_cart',$value['id'])->first();
+            $review[] = [$order];
+        }
 
         return response([
-            'data' => $order,
+            'data' => $review,
         ], 200);
     }
 
